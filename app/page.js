9925364b7bca47e38a39b0053a65e96196949fb4
unknown <shenzantech@gmail.com>
2024-08-29 14:25:39 +0500

    first commit

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <h1>
+    shenzan ali cheena is the best programmer from Thall university Bhakkar.
+   </h1>
+  );
+}
